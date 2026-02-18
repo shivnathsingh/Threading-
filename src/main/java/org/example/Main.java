d354e4ac7ddfefcaa528f3b78c46551340bae5e9
Shivnath <shivnath21085@iiitd.ac.in>
2026-02-18 07:39:45 +0530

    2nd commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,18 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        Thread t1= new Thread(new Threading());
+//        t1.start();
+
+//        Thread t2= new Thread(new Threading());
+//        t2.start();
+
+        for(int i=0;i<1000;i++)
+        {
+            Thread t= new Thread(new Threading(i));
+            t.start();
         }
+
     }
 }
--- src/main/java/org/example/Threading.java
+++ src/main/java/org/example/Threading.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+package org.example;
 
-public class Threading {
+public class Threading implements Runnable {
+
+    public int num_to_print;
+    public Threading(int num)
+    {
+        this.num_to_print=num;
+    }
+    public Threading()
+    {}
+
+    public void printVal()
+    {
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public void print_num() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(num_to_print);
+    }
+
+    @Override
+    public void run() {
+        try {
+            print_num();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+//    @Override
+//    public void run() {
+//        printVal();
+//    }
 }
